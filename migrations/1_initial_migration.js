065f5e4f9e43087e3da5bb509ed61b34242e4041
Rita Tcepeleva <34192171+RitaTcepeleva@users.noreply.github.com>
2021-02-20 17:34:01 +0300

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,5 +3,5 @@ const {TOKEN_RINKEBY_ADDR, AVS_ADDR} = process.env;
 const Migrations = artifacts.require("AVS_staking");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations, 'TOKEN_RINKEBY_ADDR or AVS_ADDR', Math.floor(Date.now()/1000), 86400);
+  deployer.deploy(Migrations, AVS_ADDR, 1613779200, 86400);
 };
